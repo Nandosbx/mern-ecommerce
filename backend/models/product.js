a95c949c77f0c1b4cbcae832dca170c9ccfa1ef1
Fernando Batista <nandosbx@gmail.com>
2020-08-20 23:40:05 -0300

    Product New

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,42 +1,49 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Schema
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 32
-  },
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32,
+        },
 
-  description: {
-    type: String,    
-    required: true,
-    maxlength: 32
-  },
+        description: {
+            type: String,
+            required: true,
+            maxlength: 32,
+        },
 
-  price: {
-    type: Number,
-    trim: true,
-    required: true,
-    maxlength: 32
-  },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+            maxlength: 32,
+        },
 
-  category: {
-    type:ObjectId,
-    required:true,
-    ref:'Category'
-  },
+        category: {
+            type: ObjectId,
+            required: true,
+            ref: 'Category',
+        },
 
-  quantity: {
-    type:Number
-  },
+        quantity: {
+            type: Number,
+        },
 
-  photo: {
-    data:Buffer,
-    contentType: String
-  }
+        sold: {
+            type: Number,
+            default: 0,
+        },
 
-}, {timestamps: true})
+        photo: {
+            data: Buffer,
+            contentType: String,
+        },
+    },
+    { timestamps: true },
+)
 
 module.exports = mongoose.model('Products', productSchema)
