e6cdb48357065db0dec3f44ab0dd31a2f9104523
Fernando Batista <nandosbx@gmail.com>
2020-08-28 12:25:21 -0300

    User Read, Update Cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 require('dotenv').config()
 
@@ -12,27 +13,27 @@ const userRouter = require('./routes/user')
 const categoryRouter = require('./routes/category')
 const productRouter = require('./routes/product')
 
-//ANCHOR Mongoose Connection 
+//ANCHOR Mongoose Connection
 mongoose
     .connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
     })
     .then(() => console.log('Database is connected'))
-    .catch(error => console.log("Database error: ", error))
+    .catch((error) => console.log('Database error: ', error))
 
 //ANCHOR Middlewares
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use(cors())
 
-
-//ANCHOR ROTAS 
-app.use('/api', authRouter    )
-app.use('/api', userRouter    )
+//ANCHOR ROTAS
+app.use('/api', authRouter)
+app.use('/api', userRouter)
 app.use('/api', categoryRouter)
 app.use('/api', productRouter)
-
 
 //ANCHOR Teste Enviado/Recebido
 /*app.use(express.json())
@@ -40,7 +41,6 @@ app.get('/', (req, res)=> {
     console.log('Recebido')
     res.json({"message": "Enviado"})
 })*/
-
 
 //ANCHOR Port
 const port = process.env.PORT || 8000
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,15 +1,43 @@
 const User = require('../models/user')
 
+//
 exports.userById = (req, res, next, id) => {
-  User.findById(id)
-    .exec((error, user) => {
-      if(error || !user) {
-        return res.status(400).json({
-          error : 'User not found'
-        })
-      }
-      //all user info will be save as req.profile
-      req.profile = user
-      next()
-    })    
+    User.findById(id).exec((error, user) => {
+        if (error || !user) {
+            return res.status(400).json({
+                error: 'User not found',
+            })
+        }
+        //all user info will be save as req.profile
+        req.profile = user
+        next()
+    })
+}
+
+//ANCHOR Read
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+
+    return res.json(req.profile)
+}
+
+//ANCHOR Update
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true },
+        (error, user) => {
+            if (error) {
+                return res.status(400).json({
+                    error: 'You are not authorized',
+                })
+            }
+
+            req.profile.hashed_password = undefined
+            req.profile.stal = undefined
+            res.json(user)
+        },
+    )
 }
