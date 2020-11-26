8ba037f93982f1fd1935205d69ca2e9f77184496
Fernando Batista <nandosbx@gmail.com>
2020-11-25 23:57:04 -0300

    Menu inserted in Layout

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './core/Home'
-import Menu from './core/Menu'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Menu />
+            
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
