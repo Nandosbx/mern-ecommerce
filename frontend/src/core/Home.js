1b8b54229366a4c3fadb08a375c08717b46885c3
Fernando Batista <nandosbx@gmail.com>
2020-11-25 23:51:11 -0300

    Readme

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Layout from './Layout'
 
 const Home = () => {
-    return <div>Home</div>
+    return <Layout title="Homepage" description="Node React E-commerce App" />
 }
 
 export default Home
