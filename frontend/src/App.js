5be273c57151ef5948c1a61ab2ce4cc1c3a83bfc
Fernando Batista <nandosbx@gmail.com>
2020-11-25 21:10:56 -0300

    Menu isActive Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Routes from './Routes'
 import './app.css'
 
-const App = () => {
-    return <div>A</div>
+function App() {
+    return <Routes />
 }
 
 export default App
