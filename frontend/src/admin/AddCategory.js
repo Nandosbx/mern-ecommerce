5301cabac9bbf5138dbf0cf21c2f3eeda454b869
Fernando Batista <nandosbx@gmail.com>
2020-11-27 20:32:22 -0300

    Add Category

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -10,16 +10,48 @@ const AddCategory = () => {
 
     const { user, token } = isAuthenticated()
 
-    const newCategoryForm = () => {
-        ;<form>
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    onChange={handleChange}
-                />
-            </div>
-        </form>
+    const handleChange = (e) => {
+        setError('')
+        setName(e.target.value)
     }
+
+    const clickSubmit = (e) => {
+        e.preventDefault()
+        setError('')
+        setSuccess(false)
+    }
+
+    const newCategoryForm = () => {
+        return (
+            <form onSubmit={clickSubmit}>
+                <div className="form-group">
+                    <label className="text-muted">Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        onChange={handleChange}
+                        value={name}
+                        autoFocus
+                    />
+                </div>
+                <button type="submit" className="btn btn-outline-primary">
+                    Create Category
+                </button>
+            </form>
+        )
+    }
+
+    return (
+        <Layout
+            title="Add a new category"
+            description={`Good day ${name}, ready to add a new category?`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+            </div>
+        </Layout>
+    )
 }
+
+export default AddCategory
