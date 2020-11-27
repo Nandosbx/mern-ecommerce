a2df26a9c1899bde4daf85b7756e3d898ec27591
Fernando Batista <nandosbx@gmail.com>
2020-11-27 15:46:19 -0300

    AddCategory created

---
+++ frontend/src/admin/AddCategory.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Layout from '../core/Layout'
+import { isAuthenticated } from '../auth'
+import { Link } from 'react-router-dom'
+
+const AddCategory = () => {
+    const [name, setName] = useState('')
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
+
+    const { user, token } = isAuthenticated()
+
+    const newCategoryForm = () => {
+        ;<form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    }
+}
