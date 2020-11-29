46a570b36371bcb2003b070e921e559b07db8f90
Fernando Batista <nandosbx@gmail.com>
2020-11-29 00:29:50 -0300

    Shop Page

---
+++ frontend/src/core/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from './Layout'
+import Card from './Card'
+
+const Shop = () => {
+    return (
+        <Layout
+            title="Shop Page"
+            description="Search and find goods of your choice"
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-4">l</div>
+                <div className="col-8">r</div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Shop
