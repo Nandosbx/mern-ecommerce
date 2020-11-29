080ed4bccb12320b5e26ede9e89b6f39d43aeab7
Fernando Batista <nandosbx@gmail.com>
2020-11-29 00:38:36 -0300

    Get Categories

--- frontend/src/core/apiCore.js
+++ frontend/src/core/apiCore.js
@@ -9,3 +9,13 @@ export const getProducts = (sortBy) => {
         })
         .catch((error) => console.log(error))
 }
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET',
+    })
+        .then((response) => {
+            return response.json({})
+        })
+        .catch((error) => console.log(error))
+}
