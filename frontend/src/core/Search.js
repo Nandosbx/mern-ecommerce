994d10a1fa44eb7c2b8d9394f15ce33869f676a7
Fernando Batista <nandosbx@gmail.com>
2020-11-29 12:10:14 -0300

    Search Form

--- frontend/src/core/Search.js
+++ frontend/src/core/Search.js
@@ -28,9 +28,60 @@ const Search = () => {
         loadCategories()
     }, [])
 
+    const searchSubmit = () => {}
+
+    const handleChange = () => {}
+
+    const searchForm = () => {
+        return (
+            <form onSubmit={searchSubmit}>
+                <span className="input-group-text bg-dark">
+                    <div className="input-group input-group-lg">
+                        <div
+                            className="input-group-prepend"
+                            style={{ border: 'none' }}
+                        >
+                            <select
+                                className="btn mr-2  text-white"
+                                onChange={handleChange('category')}
+                            >
+                                <option value="All" style={{ color: 'black' }}>
+                                    Pick Category
+                                </option>
+                                {categories.map((c, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={c._id}
+                                            style={{ color: 'black' }}
+                                        >
+                                            {c.name}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                        </div>
+                        <input
+                            type="search"
+                            className="form-control"
+                            onChange={handleChange('search')}
+                            placeholder="Search by Name"
+                        />
+                    </div>
+                    <div
+                        className="btn input-group-append"
+                        style={{ border: 'none' }}
+                    >
+                        <button className="input-group-text">Search</button>
+                    </div>
+                </span>
+            </form>
+        )
+    }
+
     return (
-        <div>
-            <h2>Search bar {JSON.stringify(categories)}</h2>
+        <div className="row">
+            <div className="container mb-3">{searchForm()}</div>
         </div>
     )
 }
