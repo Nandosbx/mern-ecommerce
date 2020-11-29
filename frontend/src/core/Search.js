fb3fed2fdbf138eb84d8f5ecf8392736ce25d010
Fernando Batista <nandosbx@gmail.com>
2020-11-29 11:04:28 -0300

    Search Created

---
+++ frontend/src/core/Search.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import Layout from './Layout'
+import { getProducts } from './apiCore'
+import Card from './Card'
+
+const Search = () => {
+    return (
+        <div>
+            <h2>Search bar</h2>
+        </div>
+    )
+}
+
+export default Search
