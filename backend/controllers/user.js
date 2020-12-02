d98f9c05496b052f5e8c614f461fe2c797649043
Fernando Batista <nandosbx@gmail.com>
2020-12-02 17:22:45 -0300

    Purchase History

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 
-//
+//NOTE User by ID
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((error, user) => {
         if (error || !user) {
@@ -14,7 +14,7 @@ exports.userById = (req, res, next, id) => {
     })
 }
 
-//ANCHOR Read
+//NOTE Read
 exports.read = (req, res) => {
     req.profile.hashed_password = undefined
     req.profile.salt = undefined
@@ -22,7 +22,7 @@ exports.read = (req, res) => {
     return res.json(req.profile)
 }
 
-//ANCHOR Update
+//NOTE Update
 exports.update = (req, res) => {
     User.findOneAndUpdate(
         { _id: req.profile._id },
@@ -38,6 +38,36 @@ exports.update = (req, res) => {
             req.profile.hashed_password = undefined
             req.profile.salt = undefined
             res.json(user)
+        },
+    )
+}
+
+exports.addOrderToUserHistory = (req, res, next) => {
+    let history = []
+
+    req.body.order.products.forEach((item) => {
+        history.push({
+            _id: item._id,
+            name: item.name,
+            description: item.description,
+            category: item.category,
+            quantity: item.count,
+            transaction_id: req.body.order.transaction_id,
+            amount: req.body.order.amount,
+        })
+    })
+
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { history: history } },
+        { new: true },
+        (error, data) => {
+            if (error) {
+                return res.status(400).json({
+                    error: 'Could not update user purchase history',
+                })
+            }
+            next()
         },
     )
 }
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const { userById } = require('../controllers/user')
+const { userById, addOrderToUserHistory } = require('../controllers/user')
 const { requireSignin, isAuth } = require('../controllers/auth')
 const { create } = require('../controllers/order')
 
-router.post('/order/create/:userId', requireSignin, isAuth, create)
+router.post(
+    '/order/create/:userId',
+    requireSignin,
+    isAuth,
+    addOrderToUserHistory,
+    create,
+)
 router.param('userId', userById)
 
 module.exports = router
