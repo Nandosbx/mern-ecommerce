156c3614aba87d4c358917b107c545488dc0eeac
Fernando Batista <nandosbx@gmail.com>
2020-12-03 14:13:21 -0300

    Show Update Status

--- frontend/src/admin/Orders.jsx
+++ frontend/src/admin/Orders.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Layout from '../core/Layout'
 import { isAuthenticated } from '../auth'
-import { listOrders } from './apiAdmin'
+import { listOrders, getStatusValues } from './apiAdmin'
 import moment from 'moment'
 
 const Orders = () => {
     const [orders, setOrders] = useState([])
-
+    const [statusValues, setStatusValues] = useState([])
     const { user, token } = isAuthenticated()
 
     const loadOrders = () => {
@@ -21,9 +21,20 @@ const Orders = () => {
         })
     }
 
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                setStatusValues(data)
+            }
+        })
+    }
+
     useEffect(() => {
         loadOrders()
-    }, [setOrders])
+        loadStatusValues()
+    }, [])
 
     const showOrdersLength = () => {
         if (orders.length > 0) {
@@ -49,6 +60,32 @@ const Orders = () => {
                     className="form-control"
                     readOnly
                 />
+            </div>
+        )
+    }
+
+    const handleStatusChange = (e, orderId) => {
+        console.log(orderId)
+    }
+
+    const showStatus = (order) => {
+        return (
+            <div className="form-group">
+                <h3 className="mark mb-3">Status: {order.status}</h3>
+
+                <select
+                    onChange={(e) => handleStatusChange(e, order._id)}
+                    className="form-control"
+                >
+                    <option>Update Status</option>
+                    {statusValues.map((status, index) => {
+                        return (
+                            <option key={index} value={status}>
+                                {status}
+                            </option>
+                        )
+                    })}
+                </select>
             </div>
         )
     }
@@ -79,7 +116,7 @@ const Orders = () => {
 
                                 <ul className="list-group mb-2">
                                     <li className="list-group-item">
-                                        {order.status}
+                                        {showStatus(order)}
                                     </li>
 
                                     <li className="list-group-item">
